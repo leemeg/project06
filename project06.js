4b1bbb774c974378320dc9b396a9c1e674b5a827
leemeg <marshalll@student.ncmich.edu>
2018-11-20 19:10:10 -0500

    works

--- project06.js
+++ project06.js
@@ -8,13 +8,9 @@
 "use strict";
 const PROMPT = require('readline-sync');
 const IO = require('fs');
-const COLUMNS = 4;
-
-let continueResponse;
-let action, servAmount, client;
-let id = 0, firstName = 1, lastName = 2, totalSpent = 3;
-let clients = [], clientsWeekly = [], rewardClients = [];
-let services = [];
+const COLUMNS = 5;
+const ID_NUM = 0, FIRST_NAME = 1, LAST_NAME = 2, TOTAL_SPENT = 3, NUM_REWARDS = 4, SERV_PERFOR = 5;
+const services = [];
 services[0] = ["Hair cut",50];
 services[1] = ["Shampoo",35];
 services[2] = ["Manicure",65];
@@ -22,57 +18,103 @@ services[3] = ["Pedicure",75];
 services[4] = ["Perm",100];
 services[5] = ["Massage",130];
 
+
+let continueResponse;
+let client;
+let clients = [], clientsWeekly = [];
+
+
 function main(){
     loadClients();
-
     if (continueResponse !== 0 && continueResponse !== 1) {
         setContinueResponse();
     }
     while (continueResponse === 1) {
-        setAction();
-        branchAction();
-
-        console.log(clients, clientsWeekly);
-        setContinueResponse();
-    }
-    //writeClients();
-}
-main();
-
-
-
-function setAction() {
-    action = -1;
-    console.log(`\x1Bc`);
-    while (action !== 1 && action !== 2 && action !== 3){
-        action = Number(PROMPT.question(
-            `What would you like to do?
+        let action;
+        console.log(`\x1Bc`);
+        while (action !== 1 && action !== 2 && action !== 3){
+            action = Number(PROMPT.question(
+                `What would you like to do?
             \t1) Enter services performed for new client
             \t2) Enter services performed for existing client
             \t3) Create weekly report
-            \tPlease enter value: `
-        ))
+            \tPlease enter value: `));
+        }
+        switch (action) {
+            case 1: addNewClient(); setService();
+                break;
+            case 2: listClients(); setClient(); setService();
+                break;
+            case 3: buildReport();
+                break;
+        }setContinueResponse();
+    }
+    checkNewData();
+    mergeData();
+
+    rewardClient();
+    writeClients();
+}
+
+main();
+
+
+function rewardClient() {
+    const AWARD_FOR = 750;
+    console.log(`\x1Bc`);
+    for (let i = 0; i < clients.length; i++){
+        if (clients[i][TOTAL_SPENT] > AWARD_FOR) {
+            if (clients[i][NUM_REWARDS] === 0){
+                clients[i][NUM_REWARDS]++;
+                console.log(`\n`);
+                console.log(`Congratulations ${clients[i][FIRST_NAME]} you have earned your first free hair cut`)
+        }
+            else if ((clients[i][TOTAL_SPENT] > (clients[i][NUM_REWARDS] * AWARD_FOR))){
+                clients[i][NUM_REWARDS]++;
+                console.log(`\n`);
+                console.log(`Congratulations ${clients[i][FIRST_NAME]} you have earned a free hair cut`)
+            }
+        }
     }
 }
 
-function branchAction() {
-    switch (action) {
-        case 1: addNewClient(); setService();
-            clientsWeekly[client][totalSpent] = clientsWeekly[client][totalSpent] + servAmount;
-            break;
-        case 2: listClients(); setClient(); setService();
-            clientsWeekly[client][totalSpent] = clientsWeekly[client][totalSpent] + servAmount;
-            break;
-        case 3: buildReport();
-            break;
+function mergeData() {
+    for (let i = 0; i < clients.length; i++) {
+        clients[i][TOTAL_SPENT] = clients[i][TOTAL_SPENT] + clientsWeekly[i][TOTAL_SPENT];
+        clients[i][NUM_REWARDS] = clients[i][NUM_REWARDS] + clientsWeekly[i][NUM_REWARDS];
     }
 }
 
 function buildReport() {
-
-
-
+    console.log(`\x1Bc`);
+    console.log(`Client #\tTotal paid\tService(s) performed\n========        ==========      ==================`);
+    for (let i = 0; i < clientsWeekly.length; i++){
+        if (clientsWeekly[i][SERV_PERFOR][0] === undefined) {
+            clientsWeekly[i][SERV_PERFOR][0] = "no service"
+        }process.stdout.write(` ${clientsWeekly[i][ID_NUM]}   \t${clientsWeekly[i][TOTAL_SPENT]}\t\t${clientsWeekly[i][SERV_PERFOR]}\n`);
+    }
 }
+
+function checkNewData() {
+    if (clientsWeekly.length > clients.length) {
+        console.log(`\x1Bc`);
+        console.log(`     ***Warning!!  New client data detected***`);
+        console.log(`The following clients were not listed in original file.`);
+        console.log(`Client #\tTotal paid\tService(s) performed\n========        ==========      ==================`);
+        for (let i = 0; i < clientsWeekly.length - clients.length; i++) {
+            if (clientsWeekly[i][SERV_PERFOR] === undefined) {
+                    clientsWeekly[i][SERV_PERFOR] = "no service"
+                }
+                process.stdout.write(` ${clientsWeekly[clients.length + i][ID_NUM]}   \t${clientsWeekly[clients.length + i][TOTAL_SPENT]} \t\t${clientsWeekly[clients.length + i][SERV_PERFOR]}\n`);
+            }
+        clients.push([]);
+        for (let j = 0; j < COLUMNS; j++) {
+            clients[clients.length - 1][j] = clientsWeekly[clients.length - 1][j];
+        }
+            PROMPT.question(`Press Enter to continue.`);
+    }
+}
+
 function setClient() {
     client = -1;
     while (!client || client < 0 || client > clientsWeekly.length) {
@@ -84,10 +126,10 @@ function setClient() {
 }
 
 function setService() {
-    const MIN_ACTION = 1, MAX_ACTION = 6;
-    let service;
+    const MIN_ACTION = 1, MAX_ACTION = 6, SERVE_STRING = 0, SERVE_VALUE = 1;
+    let service = null;
     console.log(`\x1Bc`);
-    console.log(`What service did ${clientsWeekly[client][firstName]} receive?`);
+    console.log(`What service did ${clientsWeekly[client][FIRST_NAME]} receive?`);
     for (let i = 0;i < services.length; i++){
         console.log(`${i + 1}) ${services[i][0]}`)
     }while (service == null || service > MAX_ACTION || service < MIN_ACTION || !/[0-9]/.test(service)) {
@@ -95,37 +137,40 @@ function setService() {
         if (service == null || service > MAX_ACTION || service < MIN_ACTION || !/[0-9]/.test(service)) {
                 console.log(`${service} is an incorrect value. Please try again.`)
             }
-        }servAmount = services[service-1][1];
-        return servAmount;
+        }
+    clientsWeekly[client][TOTAL_SPENT] = clientsWeekly[client][TOTAL_SPENT] + services[service-1][SERVE_VALUE];
+    clientsWeekly[client][SERV_PERFOR].push(services[service-1][SERVE_STRING]);
+
+
 }
 
 function addNewClient() {
 
-    let i = clients.length;
+    let i = clientsWeekly.length;
     clientsWeekly[i] = [];
-    clientsWeekly[i][id] = Number(clientsWeekly[i - 1][id]) + 1;
-    clientsWeekly[i][totalSpent] = 0;
-    while (!clientsWeekly[i][firstName] || !/^[a-zA-Z -]{1,30}$/.test(clientsWeekly[i][firstName])) {
-        clientsWeekly[i][firstName] = PROMPT.question(`Please enter first name: `);
-        if (!/^[a-zA-Z -]{1,30}$/.test(clientsWeekly[i][firstName])) {
-            console.log(`${clientsWeekly[i][firstName]} is invalid. Please try again.`);
+    clientsWeekly[i][ID_NUM] = Number(clientsWeekly[i - 1][ID_NUM]) + 1;
+    clientsWeekly[i][TOTAL_SPENT] = 0;
+    clientsWeekly[i][NUM_REWARDS] = 0;
+    clientsWeekly[i][SERV_PERFOR] = [];
+    while (!clientsWeekly[i][FIRST_NAME] || !/^[a-zA-Z -]{1,30}$/.test(clientsWeekly[i][FIRST_NAME])) {
+        clientsWeekly[i][FIRST_NAME] = PROMPT.question(`Please enter first name: `);
+        if (!/^[a-zA-Z -]{1,30}$/.test(clientsWeekly[i][FIRST_NAME])) {
+            console.log(`${clientsWeekly[i][FIRST_NAME]} is invalid. Please try again.`);
         }
     }
-    while (!clientsWeekly[i][lastName] || !/^[a-zA-Z -]{1,30}$/.test(clientsWeekly[i][lastName])) {
-        clientsWeekly[i][lastName] = PROMPT.question(`Please enter last name: `);
-        if (!/^[a-zA-Z -]{1,30}$/.test(clientsWeekly[i][lastName])) {
-            console.log(`${clientsWeekly[i][lastName]} is invalid. Please try again.`);
+    while (!clientsWeekly[i][LAST_NAME] || !/^[a-zA-Z -]{1,30}$/.test(clientsWeekly[i][LAST_NAME])) {
+        clientsWeekly[i][LAST_NAME] = PROMPT.question(`Please enter last name: `);
+        if (!/^[a-zA-Z -]{1,30}$/.test(clientsWeekly[i][LAST_NAME])) {
+            console.log(`${clientsWeekly[i][LAST_NAME]} is invalid. Please try again.`);
         }
-    }client = clientsWeekly.length - 1;
+    }
+    client = clientsWeekly.length - 1;
 }
 
 function listClients() {
     console.log(`\x1Bc`);
     for (let i = 0; i < clientsWeekly.length; i++) {
-        process.stdout.write(`\n${i + 1}) `);
-        for (let j = 1; j < clientsWeekly[i].length - 1; j++) {
-            process.stdout.write(`${clientsWeekly[i][j]} `)
-        }
+        console.log(`${i + 1}) ${clientsWeekly[i][FIRST_NAME]} ${clientsWeekly[i][LAST_NAME]}`);
     }
 }
 
@@ -136,18 +181,17 @@ function loadClients() {
         clients.push(lines[i].toString().split(/,/)); // Makes students array MD by pushing data between commas in
     }
     for (let j = 0; j < clients.length; j++) {
-        clients[j][id] = Number(clients[j][id]);
+        clients[j][ID_NUM] = Number(clients[j][ID_NUM]);
+        clients[j][TOTAL_SPENT] = Number(clients[j][TOTAL_SPENT]);
+        clients[j][NUM_REWARDS] = Number(clients[j][NUM_REWARDS]);
     }
     for (let k = 0; k < clients.length; k++) {
-        clients[k][totalSpent] = Number(clients[k][totalSpent]);
-    }
-    const MAX = 2;
-    for (let l = 0; l < clients.length; l++){
-        clientsWeekly[l] = [];
-        clientsWeekly[l][totalSpent] = 0;
-        for (let m = 0; m <= MAX; m++){
-            clientsWeekly[l][m] = clients[l][m];
+        clientsWeekly[k] = [];
+        for (let l = 0; l < COLUMNS; l++) {
+            clientsWeekly[k][l] = clients[k][l];
         }
+        clientsWeekly[k][TOTAL_SPENT] = 0;
+        clientsWeekly[k][SERV_PERFOR] = [];
     }
 }
 
@@ -157,7 +201,7 @@ function writeClients() {
             for (let j = 0; j < COLUMNS; j++) {
                 if (j < COLUMNS - 1) {
                     IO.appendFileSync(`data/dataX.csv`, `${clients[i][j]},`);
-                } else if (i < clients.length - 1) {
+                } else if (i < clientsWeekly.length - 1) {
                     IO.appendFileSync(`data/dataX.csv`, `${clients[i][j]}\n`);
                 } else {
                     IO.appendFileSync(`data/dataX.csv`, `${clients[i][j]}`);
